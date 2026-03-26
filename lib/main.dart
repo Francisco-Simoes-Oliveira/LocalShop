0834e39137eaa0921ac306491ee6e2fa0e8f44dd
Francisco <simoesdeoliveirafrancisco@mail.com>
2026-03-26 15:42:12 -0300

    refactor: reorganizar estrutura de pastas e remover arquivos não utilizados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shoplocal/screens/home.dart';
+import 'package:shoplocal/features/home/screens/home.dart';
 
 void main() {
   runApp(Home());
--- lib/models/Usuario.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ffi';
-
-abstract class Usuario {
-  Long id;
-  String nome;
-  String email;
-
-  Usuario(this.id, this.nome, this.email);
-}
