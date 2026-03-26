a36f11fea6b82e90adbd32d055489f086e090642
Francisco <simoesdeoliveirafrancisco@mail.com>
2026-03-26 16:26:59 -0300

    feat: adicionar novas telas de autenticação e reorganizar a estrutura de arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shoplocal/features/home/screens/home.dart';
+import 'package:shoplocal/features/home/screens/homeScreen.dart';
 
 void main() {
   runApp(Home());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shoplocal/main.dart';
+import 'package:shoplocal/features/home/screens/homeScreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Home());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
